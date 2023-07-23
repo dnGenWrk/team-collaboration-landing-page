a400b91c66838b8a7ddbc7af0527b5a0f0b48cf8
Dnworkf <general-n-dnmf@outlook.com>
2023-07-23 23:15:29 +0300

    updates

---
+++ src/components/partners/animations.js
@@ -0,0 +1,4 @@
+import gsap from "gsap";
+(function () {
+  console.log("Test Partners Animations");
+})();
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./index.html";
 import "./index.scss";
 import "./components/header/animations";
+import "./components/partners/animations";
 const pow = (a, b) => a * b;
 console.log(pow(2, 4));
