3fcbbb030aef8f6faf18954c87a75267652e1513
Dnworkf <general-n-dnmf@outlook.com>
2023-07-25 21:15:21 +0300

    updated

--- src/components/partners/animations.js
+++ src/components/partners/animations.js
@@ -1,4 +1,14 @@
 import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 (function () {
-  console.log("Test Partners Animations");
+  const elem = document.querySelector(".partners-animate");
+  gsap.from(".prtnr-1-anim", {
+    opacity: 0,
+    y: 300,
+    scrollTrigger: elem,
+    duration: 0.8,
+    ease: "power3.out",
+    stagger: 0.2,
+  });
 })();
