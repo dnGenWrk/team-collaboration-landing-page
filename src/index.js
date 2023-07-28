0ffa4e40cbddd8e47dfd16dfeec32cbf1a9e7899
Dnworkf <general-n-dnmf@outlook.com>
2023-07-28 16:25:19 +0300

    new section subscribe

--- src/index.js
+++ src/index.js
@@ -9,5 +9,6 @@ import "./components/integrate/animations";
 import "./components/save-time/animations";
 import "./components/visualize/animations";
 import "./components/customer-sup/animations";
+import "./components/subscribe/animations";
 const pow = (a, b) => a * b;
 console.log(pow(2, 4));
