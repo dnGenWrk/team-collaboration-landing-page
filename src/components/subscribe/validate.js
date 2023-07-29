f60cbd1f8b43992d6e843ae604caaf2849cb1cec
Dnworkf <general-n-dnmf@outlook.com>
2023-07-29 07:51:38 +0300

    upd

--- src/components/subscribe/validate.js
+++ src/components/subscribe/validate.js
@@ -10,18 +10,13 @@ import validator from "validator";
   function updateValue(e) {
     emailBlock.classList.remove("border-red-700");
     invalidEmailMessage.style.setProperty("visibility", "hidden");
-    console.log("!!!!");
   }
 
   cta.addEventListener("click", function () {
     const emailField = document.getElementById("subscmail").value;
-
     const isValidEmail = validator.isEmail(emailField);
-    //emailInput.classList.remove("invalid");
 
     if (!isValidEmail) {
-      console.log(emailField);
-      emailInput.classList.add("invalid");
       emailBlock.classList.add("border-red-700");
       invalidEmailMessage.style.setProperty("visibility", "visible");
       emailInput.focus();
