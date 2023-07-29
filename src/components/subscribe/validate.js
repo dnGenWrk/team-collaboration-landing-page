054acfdb997172b21458f8b114488a8339d62613
Dnworkf <general-n-dnmf@outlook.com>
2023-07-29 09:04:26 +0300

    add modal

--- src/components/subscribe/validate.js
+++ src/components/subscribe/validate.js
@@ -5,6 +5,9 @@ import validator from "validator";
   const emailInput = document.getElementById("subscmail");
   const invalidEmailMessage = document.getElementById("email_invalid_message");
   const emailBlock = document.querySelector(".subscibe-cta-block");
+  let modal = document.getElementById("my-modal");
+  //let btn = document.getElementById("open-btn");
+  let button = document.getElementById("ok-btn");
 
   emailInput.addEventListener("input", updateValue);
   function updateValue(e) {
@@ -20,6 +23,16 @@ import validator from "validator";
       emailBlock.classList.add("border-red-700");
       invalidEmailMessage.style.setProperty("visibility", "visible");
       emailInput.focus();
+    } else {
+      console.log("!!! FIRE");
+      // We want the modal to open when the Open button is clicked
+
+      modal.style.display = "block";
+
+      // We want the modal to close when the OK button is clicked
+      button.onclick = function () {
+        modal.style.display = "none";
+      };
     }
   });
 })();
