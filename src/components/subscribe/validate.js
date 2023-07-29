facd51c738942f6e139bb45c70907497f6811a68
Dnworkf <general-n-dnmf@outlook.com>
2023-07-29 07:50:46 +0300

    add validation to email field

--- src/components/subscribe/validate.js
+++ src/components/subscribe/validate.js
@@ -2,16 +2,28 @@ import validator from "validator";
 
 (function () {
   const cta = document.getElementById("emailcta");
+  const emailInput = document.getElementById("subscmail");
+  const invalidEmailMessage = document.getElementById("email_invalid_message");
+  const emailBlock = document.querySelector(".subscibe-cta-block");
+
+  emailInput.addEventListener("input", updateValue);
+  function updateValue(e) {
+    emailBlock.classList.remove("border-red-700");
+    invalidEmailMessage.style.setProperty("visibility", "hidden");
+    console.log("!!!!");
+  }
+
   cta.addEventListener("click", function () {
-    const emailInput = document.getElementById("subscmail");
     const emailField = document.getElementById("subscmail").value;
+
     const isValidEmail = validator.isEmail(emailField);
-    emailInput.classList.remove("invalid");
+    //emailInput.classList.remove("invalid");
 
     if (!isValidEmail) {
       console.log(emailField);
       emailInput.classList.add("invalid");
-      emailInput.value = "Invalid";
+      emailBlock.classList.add("border-red-700");
+      invalidEmailMessage.style.setProperty("visibility", "visible");
       emailInput.focus();
     }
   });
